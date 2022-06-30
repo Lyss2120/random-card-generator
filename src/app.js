2fbffa5fb3ab6e2f21728fb966190027676ee19a
Lyss2120 <lys.ivonne2020@gmail.com>
2022-06-29 23:01:54 -0400

    listo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(obtenerPinta()); //agrega una clase en este caso aleatoria
+  document.querySelector(".card").innerHTML = obtenerNumero(); // inserta un numero en este caso aleatorio reemplazando el que esta en html
 };
+// nueva variable contiene las dos funciones random necesarias para aparecer el nro y la pinta 
+//se activan al click!
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+  document.querySelector(".card").classList.add(obtenerPinta()); //agrega una clase en este caso aleatoria
+  document.querySelector(".card").innerHTML = obtenerNumero(); // inserta un numero en este caso aleatorio reemplazando el que esta en html
+});
+
+//se retorna un index del array nro, osea una posicion aleatoria dentro del array retorna el nro que saldrá.
+function obtenerNumero() {
+  let nro = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let nroRandom = Math.floor(Math.random() * nro.length);
+  console.log(nro[nroRandom]);
+  return nro[nroRandom];
+}
+
+function obtenerPinta() {
+  let pinta = ["heart", "club", "spade", "diamond"];
+  let pintaRandom = Math.floor(Math.random() * pinta.length);
+  return pinta[pintaRandom];
+}
